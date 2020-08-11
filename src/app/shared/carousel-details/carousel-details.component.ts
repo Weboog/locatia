79846a7a3d9b0c44a2fb27d4a47cc24c95d53415
killsyre <killsyre@yahoo.com>
2020-08-11 22:25:06 +0100

    Readjusting mobile design

--- src/app/shared/carousel-details/carousel-details.component.ts
+++ src/app/shared/carousel-details/carousel-details.component.ts
@@ -1,127 +1,145 @@
-import {AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-    selector: 'app-carousel-details',
-    templateUrl: './carousel-details.component.html',
-    styleUrls: ['./carousel-details.component.scss']
+  selector: "app-carousel-details",
+  templateUrl: "./carousel-details.component.html",
+  styleUrls: ["./carousel-details.component.scss"],
 })
 export class CarouselDetailsComponent implements OnInit, AfterViewInit {
+  @ViewChild("slider", { static: false }) slider: ElementRef;
+  @Input() elemArray: any[]; // Elements to be displayed in carousel
+  @Input() viewWidth: number; // step of slide is calculated in vw
+  @Input() itemsPerView: number;
+  id: string;
+  stepPerView: boolean;
+  loadedItems = [];
+  step: number;
+  slideWidth: number;
+  itemWidth: number;
+  i = 0; // start item
 
-    @ViewChild('slider', {static: false}) slider: ElementRef;
-    @Input() elemArray: any[]; // Elements to be displayed in carousel
-    @Input() viewWidth: number; // step of slide is calculated in vw
-    @Input() itemsPerView: number;
-    id: string;
-    stepPerView: boolean;
-    loadedItems = [];
-    step: number;
-    slideWidth: number;
-    itemWidth: number;
-    i = 0; // start item
+  constructor(private activatedRoute: ActivatedRoute) {}
 
-    constructor( private activatedRoute: ActivatedRoute) {}
+  ngOnInit(): void {
+    if (window.matchMedia('(max-width: 411px)').matches) {
+      this.viewWidth = 100;
+      this.itemsPerView = 1;
+    }
+    this.stepPerView = this.itemsPerView === 1;
+    this.activatedRoute.paramMap.subscribe((paramMap) => {
+      this.id = paramMap.get('id');
+    });
+    this.loadedItems.push(0, 1);
+    this.step = this.stepPerView
+      ? this.viewWidth
+      : this.viewWidth / this.itemsPerView;
+    this.slideWidth = this.viewWidth; // the width of screen's slide is equal to the step
+    this.itemWidth = this.slideWidth / this.itemsPerView; // we get item width from dividing screen width by number of wanted items
+  }
 
-    ngOnInit(): void {
-      if (window.matchMedia('(max-width: 411px)').matches) {
-        this.viewWidth = 100;
-        this.itemsPerView = 1;
-      }
-      this.stepPerView = this.itemsPerView === 1;
-      this.activatedRoute.paramMap.subscribe(paramMap => {
-        this.id = paramMap.get('id');
+  ngAfterViewInit(): void {
+    this.slider.nativeElement.parentNode.style.width = `${this.slideWidth}vw`; // Resize slide
+    this.slider.nativeElement.style.width = `${
+      this.itemWidth * this.elemArray.length
+    }vw`; // resize slider to be bigger or smaller
+    for (const item of this.slider.nativeElement.children) {
+      item.style.width = `${this.itemWidth}vw`; // assigning right width to each element to fit slide scope
+    }
+    if (this.stepPerView) {
+      // Removing padding left of element an+1 and an+a
+      const leftElements = document.querySelectorAll(
+        `.slider_item:nth-child(${this.itemsPerView}n+1)`
+      );
+      const rightElements = document.querySelectorAll(
+        `.slider_item:nth-child(${this.itemsPerView}n+${this.itemsPerView})`
+      );
+
+      leftElements.forEach((elem: HTMLDivElement) => {
+        elem.style.paddingLeft = String(0);
       });
-      this.loadedItems.push(0, 1);
-      this.step = this.stepPerView ? this.viewWidth : this.viewWidth / this.itemsPerView;
-      this.slideWidth = this.viewWidth; // the width of screen's slide is equal to the step
-      this.itemWidth = this.slideWidth / this.itemsPerView; // we get item width from dividing screen width by number of wanted items
+      rightElements.forEach((elem: HTMLDivElement) => {
+        elem.style.paddingRight = String(0);
+      });
     }
+  }
 
-    ngAfterViewInit(): void {
-        this.slider.nativeElement.parentNode.style.width = `${this.slideWidth}vw`; // Resize slide
-        this.slider.nativeElement.style.width = `${this.itemWidth * this.elemArray.length}vw`; // resize slider to be bigger or smaller
-        for (const item of this.slider.nativeElement.children) {
-            item.style.width = `${this.itemWidth}vw`; // assigning right width to each element to fit slide scope
-        }
-        if (this.stepPerView) {
-            // Removing padding left of element an+1 and an+a
-            const leftElements = document.querySelectorAll(`.slider_item:nth-child(${this.itemsPerView}n+1)`);
-            const rightElements = document.querySelectorAll(`.slider_item:nth-child(${this.itemsPerView}n+${this.itemsPerView})`);
-
-            leftElements.forEach((elem: HTMLDivElement) => {
-                elem.style.paddingLeft = String(0);
-            });
-            rightElements.forEach((elem: HTMLDivElement) => {
-                elem.style.paddingRight = String(0);
-            });
-        } else {
-
-        }
+  move(direction: string) {
+    const slider = this.slider.nativeElement;
+    let length = this.elemArray.length / this.itemsPerView;
+    if (!this.stepPerView) {
+      if (this.itemsPerView > 1) {
+        length = this.elemArray.length - (this.itemsPerView - 1);
+      }
     }
-
-    move(direction: string) {
-        const slider = this.slider.nativeElement;
-        let length = this.elemArray.length / this.itemsPerView;
-        if (!this.stepPerView) {
-            if (this.itemsPerView > 1) {
-                 length = this.elemArray.length - ( this.itemsPerView - 1 );
+    while (this.i < length) {
+      switch (direction) {
+        case 'right':
+          if (this.i + 1 < length) {
+            this.i++;
+            if (
+              this.elemArray.includes(this.i + 1) &&
+              !this.loadedItems.includes(this.i + 1)
+            ) {
+              this.sliderItemCreator(slider, this.i);
+              // Remember loaded items
+              this.loadedItems.push(this.i + 1);
             }
-        }
-        while (this.i < length) {
-            switch (direction) {
-                case 'right':
-                    if (this.i + 1 < length) {
-                        this.i++;
-                        if (this.elemArray.includes(this.i + 1) && !this.loadedItems.includes(this.i + 1)) {
-                          this.sliderItemCreator(slider, this.i);
-                          // Remember loaded items
-                          this.loadedItems.push(this.i + 1);
-                        }
-                    }
-                    slider.style.marginLeft = `calc( -${this.step}vw * ${this.i})`;
-                    break;
-                case 'left':
-                    if (this.i - 1 >= 0) {
-                        this.i--;
-                    }
-                    slider.style.marginLeft = `calc( -${this.step}vw * ${this.i})`;
-            }
-            break;
-        }
+          }
+          slider.style.marginLeft = `calc( -${this.step}vw * ${this.i})`;
+          break;
+        case "left":
+          if (this.i - 1 >= 0) {
+            this.i--;
+          }
+          slider.style.marginLeft = `calc( -${this.step}vw * ${this.i})`;
+      }
+      break;
     }
+  }
 
   private sliderItemCreator(hostElement: HTMLDivElement, currentItem: number) {
     // PROGRAMMATIC HTML -----------------------------------
-      const sliderItem = document.createElement('div');
-      // Styling the placeholder dynamically
-      sliderItem.className = 'slider_item';
-      sliderItem.style.width = `${this.itemWidth}vw`;
-      sliderItem.style.padding = window.matchMedia('(max-width: 411px)').matches ?  '0' : '0 .5rem';
-      sliderItem.style.display = 'flex';
-      sliderItem.style.justifyContent = 'center';
-      sliderItem.style.alignItems = 'center';
-      sliderItem.style.backgroundColor = '#FAFAFA';
-      sliderItem.style.backgroundClip = 'content-box';
-      sliderItem.innerHTML = `<div style="border-radius: 2rem; overflow: hidden; height: .5rem"><img src="assets/loaders/15.gif"></div>`;
-      // Calling lazyLoading function to load and append image to sliderPlaceHolder
-      this.lazyLoadImage(sliderItem, currentItem);
+    const sliderItem = document.createElement("div");
+    // Styling the placeholder dynamically
+    sliderItem.className = 'slider_item';
+    sliderItem.style.width = `${this.itemWidth}vw`;
+    sliderItem.style.padding = window.matchMedia("(max-width: 411px)").matches
+      ? "0"
+      : "0 .5rem";
+    sliderItem.style.display = "flex";
+    sliderItem.style.justifyContent = "center";
+    sliderItem.style.alignItems = "center";
+    sliderItem.style.backgroundColor = "#FAFAFA";
+    sliderItem.style.backgroundClip = "content-box";
+    sliderItem.innerHTML = `<div style="border-radius: 2rem; overflow: hidden; height: .5rem"><img src="assets/loaders/15.gif"></div>`;
+    // Calling lazyLoading function to load and append image to sliderPlaceHolder
+    this.lazyLoadImage(sliderItem, currentItem);
     // ADDING ELEMENTS TO VIEW -----------------------------
-      hostElement.append(sliderItem);
+    hostElement.append(sliderItem);
   }
 
   private lazyLoadImage(hostElement: HTMLDivElement, currentItem: number) {
     const imageObject = new Image();
-    const imgSrc = `assets/media/gallery/${this.id}/${this.id}-${currentItem + 1}.jpg`;
-    imageObject.addEventListener('load', () => {
+    const imgSrc = `assets/media/gallery/${this.id}/${this.id}-${
+      currentItem + 1
+    }.jpg`;
+    imageObject.addEventListener("load", () => {
       // this.isLoading = false;
       // setTimeout( () => {
-        hostElement.appendChild(imageObject);
+      hostElement.appendChild(imageObject);
       // }, 3000);
     });
     imageObject.alt = `Image-${currentItem + 1}`;
     imageObject.src = imgSrc;
-    imageObject.style.alignSelf = 'flex-start';
-    imageObject.style.width = '100%';
+    imageObject.style.alignSelf = "flex-start";
+    imageObject.style.width = "100%";
   }
-
 }
