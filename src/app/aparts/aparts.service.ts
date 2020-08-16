12bff6d3f21c9028109e93d72b3fbbd10c85523c
killsyre <killsyre@yahoo.com>
2020-08-16 13:14:48 +0100

    Refactoring featured apart

--- src/app/aparts/aparts.service.ts
+++ src/app/aparts/aparts.service.ts
@@ -11,14 +11,10 @@ export class ApartsService {
   aparts: Apart[];
   featuredAparts = [
     {
-      id: '',
       human_id: 'B-0387',
-      oldPrice: 2200,
       price: 1900,
       period: 'mois',
-      property: 'maison',
       city: 'kenitra',
-      phone: '0626629199',
       surface: 80,
       pieces: 4,
       rooms: 3,
@@ -28,18 +24,13 @@ export class ApartsService {
         {id: 2, src: 'http://deelay.me/1000/https://cdn.pixabay.com/photo/2015/03/26/09/42/bedroom-690129_960_720.jpg'},
         {id: 3, src: 'https://cdn.pixabay.com/photo/2015/10/12/15/00/room-984076_960_720.jpg'},
         {id: 4, src: 'https://cdn.pixabay.com/photo/2020/03/09/23/59/istanbul-4917447_960_720.jpg'},
-      ],
-      // tslint:disable-next-line:max-line-length
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      ]
     },
     {
-      id: '',
       human_id: 'C-0037',
       price: 3400,
       period: 'mois',
-      property: 'duplexe',
       city: 'casablanca',
-      phone: '0626629199',
       surface: 75,
       pieces: 3,
       rooms: 2,
@@ -50,18 +41,13 @@ export class ApartsService {
         {id: 4, src: 'https://cdn.pixabay.com/photo/2018/05/25/17/52/home-3429674_960_720.jpg'},
         {id: 5, src: 'https://cdn.pixabay.com/photo/2017/02/24/12/22/apartment-2094702_960_720.jpg'},
         {id: 6, src: 'https://cdn.pixabay.com/photo/2016/02/18/22/25/window-1208277_960_720.jpg'}
-      ],
-      // tslint:disable-next-line:max-line-length
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      ]
     },
     {
-      id: '',
       human_id: 'D-4033',
       price: 600,
       period: 'nuité',
-      property: 'villa',
       city: 'rabat',
-      phone: '0626629199',
       surface: 75,
       pieces: 3,
       rooms: 2,
@@ -72,9 +58,7 @@ export class ApartsService {
         {id: 4, src: 'https://cdn.pixabay.com/photo/2018/05/25/17/52/home-3429674_960_720.jpg'},
         {id: 5, src: 'https://cdn.pixabay.com/photo/2017/02/24/12/22/apartment-2094702_960_720.jpg'},
         {id: 6, src: 'https://cdn.pixabay.com/photo/2016/02/18/22/25/window-1208277_960_720.jpg'}
-      ],
-      // tslint:disable-next-line:max-line-length
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+      ]
     }
   ];
   registeredAparts: Apart[] = [];
