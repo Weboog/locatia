cb9f0865b85f320fafa8e3f0b6890c643bb61294
Joebeurg <joebeurg@outlook.com>
2020-08-20 18:19:23 +0100

    Implementing the new dynamic slider

--- src/app/shared/slider/slider.service.ts
+++ src/app/shared/slider/slider.service.ts
@@ -5,25 +5,13 @@ import { SlideItem } from './slide-item';
   providedIn: 'root',
 })
 export class SliderService {
-
   cardsArray = [];
 
   public getSlides(component, data) {
-
-    for (const dt of data) {
-      this.cardsArray.push(new SlideItem(component, dt));
+    for (const item of data) {
+      this.cardsArray.push(new SlideItem(component, item));
       console.log(data);
     }
     return this.cardsArray;
-
-    // data.forEach((item) => {
-      // this.cardsArray.push(new SlideItem(component, item));
-    // });
-
-    // return [
-    //   new SlideItem(component, data[0]),
-    //   new SlideItem(component, data[1]),
-    //   new SlideItem(component, data[2]),
-    // ];
   }
 }
